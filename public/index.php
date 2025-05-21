e61dad4660dc4f9b4ac9e6c360112200405dd4e5
Richard Vass <vassrichard31@gmail.com>
2025-05-21 17:12:16 +0200

    Few changes in files

--- public/index.php
+++ public/index.php
@@ -1,1 +1,23 @@
+<!-- public/index.php -->
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <title>Bootstrap Test</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet"
+          href="../vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+</head>
+<body>
+
+<div class="container mt-5">
+    <h1 class="text-center">Hello, Bootstrap!</h1>
+    <button class="btn btn-primary">Klikk ide</button>
+</div>
+
+<!-- Bootstrap JS és Popper (Bootstrap 5 esetén a bundle tartalmazza) -->
+<script src="../vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
 <?php
